31d00580ae876ab206169613f2e74085628acda8
Adam Saunders <saunders@cs.umass.edu>
2014-09-02 16:57:44 -0400

    possibly bad changes

--- .emacs-live.el
+++ .emacs-live.el
@@ -1,5 +1,15 @@
 ;; symlink to user home to override emacs live defaults (packs etc)
 
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (package-initialize)                ;; Initialize & Install Package
+  )
+
+
 ;; Default
 ;;(live-use-packs '(
 ;;                  stable/foundation-pack
